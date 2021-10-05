7cc0c6f2aeebad483ea7cd66070aaf1238ec6ca0
uksahoo <upendrakumar.sahoo@fluidra.com>
2021-10-05 22:35:21 +0530

    Basic code

---
+++ eclipse-workspace/UI_Automation_Task/src/main/java/Test_Automation/UI_Automation_Task/App.java
@@ -0,0 +1,13 @@
+package Test_Automation.UI_Automation_Task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
