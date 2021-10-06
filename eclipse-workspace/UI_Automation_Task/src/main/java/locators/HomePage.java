2329b51470cea5087cdb359ed4d3173d594bdea2
uksahoo <upendrakumar.sahoo@fluidra.com>
2021-10-06 13:33:12 +0530

    Updated Home page

--- eclipse-workspace/UI_Automation_Task/src/main/java/locators/HomePage.java
+++ eclipse-workspace/UI_Automation_Task/src/main/java/locators/HomePage.java
@@ -1,10 +1,13 @@
 package locators;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import drivercore.DriverFactory;
 
@@ -13,12 +16,53 @@ public class HomePage extends DriverFactory {
 
 	@FindBy(xpath = "//*[text()='Welcome to the Simple Travel Agency!']")
 	WebElement pageTitle;
+	
+	@FindBy(xpath = "//select[@name='fromPort']")
+	WebElement fromCity;
+	
+	@FindBy(xpath = "//select[@name='toPort']")
+	WebElement toCity;
+	
+	@FindBy(xpath = "//input[@type='submit']")
+	WebElement flightSearchButton;
 
 	public HomePage() {
 		PageFactory.initElements(driver.get(), this);
 	}
 
 	public void verifyPageTitle() {
-		assertEquals(pageTitle.getText(), "Welcome to the Simple Travel Agency!");
+		assertTrue(pageTitle.getText().equals("Welcome to the Simple Travel Agency!"));
 	}
+	
+	public void verifyFromCityDisplayedAndEnabled() {
+		assertTrue(fromCity.isDisplayed());
+		assertTrue(fromCity.isEnabled());
+	}
+	
+	public void clickAndSelectFromCity(String fCity) {
+		Select selectFromCity = new Select(fromCity);
+		fromCity.click();
+		selectFromCity.selectByVisibleText(fCity);
+	}
+	
+	public void verifyToCityDisplayedAndEnabled() {
+		assertTrue(toCity.isDisplayed());
+		assertTrue(toCity.isEnabled());
+	}
+	
+	public void clickAndSelectToCity(String tCity) {
+		Select selectFromCity = new Select(toCity);
+		toCity.click();
+		selectFromCity.selectByVisibleText(tCity);
+	}
+	
+	public void clickOnFlightSearchButton() {
+		flightSearchButton.click();
+	}
+	
+	public void verifyListOfFlightShown() {
+		assertTrue(driver.get().findElements(By.xpath("//input[@value='Choose This Flight']")).size()> 0);
+	}
+	
+	
 }
--- eclipse-workspace/UI_Automation_Task/src/test/java/Test_Automation/UI_Automation_Task/TestScenario.java
+++ eclipse-workspace/UI_Automation_Task/src/test/java/Test_Automation/UI_Automation_Task/TestScenario.java
@@ -2,12 +2,6 @@ package Test_Automation.UI_Automation_Task;
 
 import static org.testng.Assert.assertTrue;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,15 +10,12 @@ import locators.HomePage;
 
 public class TestScenario extends DriverFactory {
 
-	WebElement pageTitle = driver.get().findElement(By.xpath("//*[text()='Welcome to the Simple Travel Agency!']"));
-	WebElement fromCity = driver.get().findElement(By.xpath("//select[@name='fromPort']"));
-	Select selectFromCity = new Select(fromCity);
-	WebElement toCity = driver.get().findElement(By.xpath("//select[@name='toPort']"));
-	Select selectToCity = new Select(toCity);
-	WebElement findFlights = driver.get().findElement(By.xpath("//input[@type='submit']"));
-	
-	public TestScenario() {
-		PageFactory.initElements(driver.get(), this);
+	public HomePage home;
+
+	@BeforeTest(alwaysRun = true)
+
+	public void initilization() {
+		home = new HomePage();
 	}
 
 	@Test(description = "Verify page is navigating to landing screen and validate page contents")
@@ -32,26 +23,20 @@ public class TestScenario extends DriverFactory {
 
 		System.out.println("Loading URL........");
 		assertTrue(driver.get().getTitle().equals("BlazeDemo"));
-		assertTrue(pageTitle.getText().equals("Welcome to the Simple Travel Agency!"));
-		assertTrue(fromCity.isDisplayed());
-		assertTrue(fromCity.isEnabled());
-		assertTrue(toCity.isDisplayed());
-		assertTrue(toCity.isEnabled());
+		home.verifyPageTitle();
+		home.verifyFromCityDisplayedAndEnabled();
+		home.verifyToCityDisplayedAndEnabled();
 	}
 
 	@Test(description = "Select the City and search flight")
 	public void scenario2() throws InterruptedException {
-
-		List<WebElement> listOfFlights = driver.get().findElements(By.xpath("//input[@value='Choose This Flight']"));
 		// Step 1:
-		fromCity.click();
-		selectFromCity.selectByVisibleText("Boston");
+		home.clickAndSelectFromCity("Boston");
 		// Step 2:
-		toCity.click();
-		selectToCity.selectByVisibleText("Rome");
+		home.clickAndSelectToCity("Rome");
 		// Step 3:
-		findFlights.click();
-		Thread.sleep(5000);
-		assertTrue(driver.get().findElements(By.xpath("//input[@value='Choose This Flight']")).size() > 0);
+		home.clickOnFlightSearchButton();
+		Thread.sleep(10);
+		home.verifyListOfFlightShown();
 	}
 }
